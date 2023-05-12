f84c7e507c04887b12941f9db028e4f0ed24bb86
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2023-05-12 20:57:30 +0200

    Update build-script.sh

--- build-script.sh
+++ build-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instalacja modułu decouple
-pip install python-decouple -t ./python_packages
+pip install python-decouple -t ./python_packages/lib/python3.9/site-packages
 
 # Przekazanie kontroli dalej do wersji @vercel/python
 vercel-python "$@"
