5112e83c4af6ddf36895a128e00b7414a2db39a0
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2023-05-13 12:50:44 +0200

    Update urls.py

--- wheelShop/urls.py
+++ wheelShop/urls.py
@@ -21,3 +21,6 @@ urlpatterns=[
 
     
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
