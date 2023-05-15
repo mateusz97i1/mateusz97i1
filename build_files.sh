9d6670eeaa397c12661dc77f112ae11e1060d8cc
mateusz97i1 <120130259+mateusz97i1@users.noreply.github.com>
2023-05-15 21:41:11 +0200

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,15 @@
 # build_files.sh
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+
+# Kompilacja plików źródłowych
+python setup.py build
+
+# Wykonaj migracje bazy danych
+python manage.py migrate
+
+# Zebranie statycznych plików
+python manage.py collectstatic --noinput
+
+# Uruchomienie testów jednostkowych
+python manage.py test
